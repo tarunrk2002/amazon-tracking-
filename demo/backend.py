c4a4f06a530e0f5e507dbf2b170907dd34a18c8f
tarunrk2002 <55913106+tarunrk2002@users.noreply.github.com>
2021-12-07 21:43:55 +0530

    backend sql comments

---
+++ demo/backend.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+def connect():
+    con=sqlite3.connect("book.db")
+    cur=con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (email text)")
+    con.commit()
+    con.close()
+
+def insert(email):
+    con = sqlite3.connect("book.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO book VALUES(?)",(email,))
+    con.commit()
+    con.close()
+
+def view():
+        con = sqlite3.connect("book.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM book")
+        rows=cur.fetchall()
+        con.close()
+        return rows
+
+def search(email=""):
+    con = sqlite3.connect("book.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM book WHERE email=?",(email,))
+    rows = cur.fetchall()
+    con.close()
+    return rows
+def delete(email):
+    con = sqlite3.connect("book.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM book WHERE email=?", (email,))
+    con.commit()
+    con.close()
+def update(email):
+    con = sqlite3.connect("book.db")
+    cur = con.cursor()
+    cur.execute("UPDATE book SET email=? ",(email,))
+    con.commit()
+    con.close()
+connect()
+
+# insert("tarunrk2002@gmail.com")
+# delete('')
+# search("kushvithchinna900@gmail.com")
+# print(view()) returning ...
+def convertTuple(tup):
+    str =  ''.join(tup)
+    return str
+def searchings(view):
+    for email in view:
+     converted = convertTuple(email)
+     return converted
+
+
+
+# searching(view())
